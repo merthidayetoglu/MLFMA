622211f05c7fd0bd15a06c3fb45df62cc077d7e2
Mert Hidayetoglu <merth@login3.summit.olcf.ornl.gov>
2021-11-04 22:34:45 -0400

    read & scale model file

--- main.cpp
+++ main.cpp
@@ -1,11 +1,72 @@
-#include "main.h"
+#include <stdio.h>
+#include <mpi.h>
+#include <omp.h>
 
-int main()
-{
-    read_simulationinput();
-    read_meshmodel();
-    genetare_constants();
-//    generate_octtree();
+double timetotal;
+double timetemp;
 
-    return 0;
+double leafbox;
+
+int numnode;
+double *nodepos;
+
+int myrank;
+int numrank;
+int numthread;
+
+int main(int argc, char** argv) {
+
+  timetotal = MPI_Wtime();
+
+  MPI_Init(&argc,&argv);
+  MPI_Comm_rank(MPI_COMM_WORLD,&myrank);
+  MPI_Comm_size(MPI_COMM_WORLD,&numrank);
+
+  #pragma omp parallel
+  if(omp_get_thread_num() == 0)
+    numthread = omp_get_num_threads();
+
+  //INPUT PARAMETERS
+  char *chartemp;
+  chartemp = getenv("LEAFBOX");
+  double leafbox = atof(chartemp);
+  char *modelfile = getenv("MODELFILE");
+  chartemp = getenv("SCALE");
+  double scalefactor = atof(chartemp);
+
+  //REPORT INPUT PARAMETERS
+  if(myrank == 0){
+    printf("LEAF-LEVEL BOX SIZE: %e\n",leafbox);
+    printf("MODEL FILE: %s\n",modelfile);
+    printf("SCALE FACTOR: %e\n",scalefactor);
+    printf("\n");
+    printf("NUMBER OF PROCESSES: %d\n",numrank);
+    printf("NUMBER OF THREADS: %d\n",numthread);
+    printf("\n");
+  }
+
+  //READ GEOMETRY
+  MPI_Barrier(MPI_COMM_WORLD);
+  timetemp = MPI_Wtime();
+  if(myrank == 0)printf("MODEL FILE: %s\n",modelfile);
+  if(myrank == 0)printf("SCALE: %e\n",scalefactor);
+  FILE *pFile = fopen(modelfile,"rb");
+  fread(&numnode,sizeof(int),1,pFile);
+  if(myrank == 0)printf("NUMBER OF NODES: %d (%f GB)\n",numnode,numnode*sizeof(double)*3/1.0e9);
+  nodepos = new double[numnode*3];
+  fread(nodepos,sizeof(double),numnode*3,pFile);
+  fclose(pFile);
+  for(int n = 0; n < numnode*3; n++)
+    nodepos[n] *= scalefactor;
+  MPI_Barrier(MPI_COMM_WORLD);
+  if(myrank == 0)printf("INPUT TIME: %e\n",MPI_Wtime()-timetemp);
+
+  //CONSTRUCT TREE STRUCTURE
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  if(myrank == 0)printf("TOTAL TIME: %e\n",MPI_Wtime()-timetotal);
+
+  MPI_Finalize();
+
+  return 0;
 }
--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 date
-module load cuda
+
+export LEAFBOX=0.25
+export MODELFILE=/gpfs/alpine/csc362/scratch/merth/MLFMA/sphere_0p01m.vtk
+export SCALE=1.0;
 
 #mpirun -n 1 -N 1 -x OMP_NUM_THREADS=40 -x OMP_PLACES=cores ./MLFMA
 jsrun -n1 -a1 -g1 -c21 -EOMP_NUM_THREADS=21 -r1 -bpacked:21 js_task_info ./MLFMA
